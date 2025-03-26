059d26d1d0c464870fa0f11c589241ca5436b4f0
JacksonVirgo <jacknash42@gmail.com>
2025-03-26 21:26:16 +1000

    chore: setup server

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -2,6 +2,16 @@ function App() {
   return (
     <>
       <h1 className="text-red-500">Tailwind Test</h1>
+
+      {/* 
+    
+        - Chat box
+        - Player radial menu
+        - Role list
+        - Timers
+        - Settings
+
+      */}
     </>
   );
 }
